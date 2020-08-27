a0b248b032efae8f778095ace168ed8d1b97a9cf
guyuezhai <guyue_fei@163.com>
2020-08-27 16:22:58 +0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -4,11 +4,11 @@ const config =require('../config');
 const pool = new pg.Pool(config.db);
 
 pool.on('error', function(err,client){
-    console.error('GeoHey Cloud连接数据出错',err);
+    console.error('连接数据出错',err);
 });
 
 pool.on('connect', cl =>{
-    console.log('GeoHey Cloud服务已经连接数据库');
+    console.log('服务已经连接数据库');
 });
 
 const query = async function (sql , params ){
